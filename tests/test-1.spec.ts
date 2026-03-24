f6cf29506009f32c51dbe05b9d196d760775fade
sokol <sokolovisokolovi@yandex.ru>
2026-03-24 23:02:05 +0300

    Initial commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    
+
+await page.goto('https://playwright.dev/');
+
+await page.getByRole('link', { name: 'Get started' }).click();
+await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+await page.goto('https://playwright.dev/');
+});
